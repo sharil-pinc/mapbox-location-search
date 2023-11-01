7f65c4b830a1d9bec35888e877f3830040e10f0f
Sharil Sarifhudin <sharil@pinc.my>
2023-11-01 12:23:22 +0800

    Fix maps

--- src/Map.js
+++ src/Map.js
@@ -5,16 +5,18 @@ import mapboxgl from 'mapbox-gl';
 mapboxgl.accessToken = 'pk.eyJ1IjoibmF6cnVsLWp1Z2dlcm5heiIsImEiOiJjbGxudWd3c2wwM282M2VvNHI3bWsyY3ViIn0.tIwOlxD2FcESclugJAr98A';
 
 const Map = ({ locationData }) => {
+    console.log({locationData});
   useEffect(() => {
     const map = new mapboxgl.Map({
       container: 'map',
       style: 'mapbox://styles/mapbox/streets-v11',
-      center: [locationData.longitude, locationData.latitude],
+    //   center: [locationData.longitude, locationData.latitude],
+      center: [102.172047, 2.456932],
       zoom: 10,
     });
-
+    
     // Add a marker for the location
-    new mapboxgl.Marker().setLngLat([locationData.longitude, locationData.latitude]).addTo(map);
+      new mapboxgl.Marker().setLngLat([101.6942371, 3.1516964]).addTo(map);
   }, [locationData]);
 
   return <div id="map" style={{ width: '100%', height: '400px' }}></div>;
